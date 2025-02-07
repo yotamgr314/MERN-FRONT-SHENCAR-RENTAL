43e986ca1a8142dfd1813172316876ac4b49389c
Yotam Greenstein <yotamgr314@gmail.com>
2025-02-07 21:03:26 +0200

    Setup React project structure
    1) Created React project using Create React App
    - Set up project folder structure:
      - frontend/src/components
      -backend/data/shenkarCar.data
    2) Configured .gitignore to exclude node_modules
    3) removed unnnencsery code from the default npx-create-react- app command.

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { BrowserRouter as Router, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <Routes></Routes>
+    </Router>
+  );
+}
+
+export default App;
