9f519d7275aeb5208f3fc117305f7a8749773469
Yotam Greenstein <yotamgr314@gmail.com>
2025-02-08 17:17:06 +0200

    footer.js - restructured
    changed the structure of footer.js.
    to one component.

--- frontend/src/components/footer/footer.js
+++ frontend/src/components/footer/footer.js
@@ -1,20 +1,57 @@
-// src/components/Footer/Footer.jsx
 import React from "react";
-import FooterLogo from "./FooterLogo";
-import FooterLinks from "./FooterLinks";
-import FooterBottom from "./FooterBottom";
-import "./footerStyle.css";
+import Logo from "../Logo/Logo";
+import "./footer.css";
 
-const Footer = () => {
+function Footer() {
   return (
     <footer className="footer">
       <div className="footer-top">
-        <FooterLogo />
-        <FooterLinks />
+        <div className="footer-logo">
+          <Logo />
+          <p>
+            Our vision is to provide convenience and help increase your sales
+            business.
+          </p>
+        </div>
+        <div className="footer-links">
+          <div className="footer-column">
+            <h3>About</h3>
+            <ul>
+              <li>How it works</li>
+              <li>Featured</li>
+              <li>Partnership</li>
+              <li>Business Relation</li>
+            </ul>
+          </div>
+          <div className="footer-column">
+            <h3>Community</h3>
+            <ul>
+              <li>Events</li>
+              <li>Blog</li>
+              <li>Podcast</li>
+              <li>Invite a friend</li>
+            </ul>
+          </div>
+          <div className="footer-column">
+            <h3>Socials</h3>
+            <ul>
+              <li>Discord</li>
+              <li>Instagram</li>
+              <li>Twitter</li>
+              <li>Facebook</li>
+            </ul>
+          </div>
+        </div>
       </div>
-      <FooterBottom />
+      <div className="footer-bottom">
+        <p>©2025 ShenCarCar. All rights reserved</p>
+        <div className="footer-bottom-links">
+          <a href="/privacy">Privacy & Policy</a>
+          <a href="/terms">Terms & Condition</a>
+        </div>
+      </div>
     </footer>
   );
-};
+}
 
 export default Footer;
