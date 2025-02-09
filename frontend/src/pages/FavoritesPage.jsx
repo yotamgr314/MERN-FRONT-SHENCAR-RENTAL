77c67dc3dad7563b81fb922b528d6dd70076aa57
Yotam Greenstein <yotamgr314@gmail.com>
2025-02-09 05:21:47 +0200

    refactored FavoritePages.jxs
    refactored the FavoritePages logic into a seperate compoenent
    1) Displays user's favorite cars
    2) Filters cars by saved favorites
    3) Uses CarList component for rendering

---
+++ frontend/src/pages/FavoritesPage.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import CarList from "../components/carList/CarList";
+
+function FavoritesPage({ cars, favorites, toggleFavorite }) {
+  const favoriteCars = cars.filter((car) => favorites.includes(car.id));
+
+  return (
+    <div className="home-container">
+      <header className="catalog-header">
+        <h1>Favorites</h1>
+        <p>{favoriteCars.length} Cars</p>
+      </header>
+      <CarList
+        cars={favoriteCars}
+        favorites={favorites}
+        toggleFavorite={toggleFavorite}
+      />
+    </div>
+  );
+}
+
+export default FavoritesPage;
